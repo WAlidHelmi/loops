f51bcdb33427941bec88f89cd03548bd7039f599
WAlidHelmi <113910191+WAlidHelmi@users.noreply.github.com>
2022-10-05 16:18:22 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 9; index++) {
-        number += 1
+        number += -1
         basic.showNumber(number)
     }
 })
 let number = 0
-number = 0
+number = 9
 basic.showNumber(number)
